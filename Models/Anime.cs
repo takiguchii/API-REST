16be7eb9c6d1b942d9a79b4818da932dbe780c2c
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-09 17:10:57 -0300

    feat: setup inicial do projeto

---
+++ Models/Anime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConectDatabase;
+
+public partial class Anime
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public DateTime DataLancamento { get; set; }
+}
