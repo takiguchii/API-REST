b4f050bfdf828b6146d24a3232afb0487dd522a5
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-10 18:22:23 -0300

    Fix: Corrigindo foreing key do Anime.cs

--- Models/Anime.cs
+++ Models/Anime.cs
@@ -6,8 +6,8 @@ namespace ConectDatabase;
 public partial class Anime
 {
     public int Id { get; set; }
-
     public string Nome { get; set; } = null!;
-
     public DateTime DataLancamento { get; set; }
+    public int GeneroId { get; set; }
+    public Genero Genero { get; set; }
 }
