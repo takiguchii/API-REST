c587b468332b860132a2e18e7d5aaff5fa01f7eb
Takiguchii <alexandre.pupim360@gmail.com>
2025-08-09 21:11:36 -0300

    Feat: Adicionando conexão do DbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
-namespace ConectDatabase;
+namespace ConectDatabase.Data;
 
 public partial class AppDbContext : DbContext
 {
--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+using ConectDatabase;
+using ConectDatabase.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//adicionando o nosso DbContext, usando a string de conexao
+var connectionString = "Server=Local;User Id=root;Password=170918;Database=Anime_db";
+builder.Services.AddDbContext<AppDbContext>(options=>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
@@ -14,5 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
